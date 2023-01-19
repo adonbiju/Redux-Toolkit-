32223d350c50633564553a54ce95e493fde3accb
adonbiju <adonbiju@gmail.com>
2023-01-19 17:20:51 +0530

    Added Slice Reducers to the Store

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,7 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import todosReducer from '../state'
 export default configureStore({
-  reducer: {},
+  reducer: {
+    todos: todosReducer
+  },
 });
