bf9e494838a0ffac254d3acfd0a7bec93d7c5697
adonbiju <adonbiju@gmail.com>
2023-01-19 17:15:56 +0530

    Redux toolkit State Slice has been created

---
+++ src/state/index.js
@@ -0,0 +1,22 @@
+import { createSlice } from '@reduxjs/toolkit';
+import { v4 as uuid } from 'uuid';
+
+export const todosSlice = createSlice({
+  name: 'todos',
+  initialState: [],
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: uuid(),
+        text: action.payload,
+      };
+
+
+      return [...state, todo];
+    }
+  },
+});
+
+export const { addTodo} = todosSlice.actions;
+
+export default todosSlice.reducer;
